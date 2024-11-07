f27ab0e7c9f5bda84f5c85e3e437feb94e9219db
Emanuel <101946589+Emanuel3queijos@users.noreply.github.com>
2024-11-07 03:50:03 -0300

    adicioanando csvs 30 mil alunos

--- back-front/gestao-universitaria/src/main/java/edu/ucsal/gestaouniversitaria/repositories/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ucsal.gestaouniversitaria.repositories;
-
-import edu.ucsal.gestaouniversitaria.model.Disciplina;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
-}
